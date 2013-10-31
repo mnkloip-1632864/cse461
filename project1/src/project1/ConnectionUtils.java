6aab3bf721c741da3f211e376fd2adcae17177eb
Joseph Godlewski <joe3701@gmail.com>
2013-10-31 16:52:42 -0700

    Added concatenation method for byte arrays

--- project1/src/project1/ConnectionUtils.java
+++ project1/src/project1/ConnectionUtils.java
@@ -1,5 +1,7 @@
 package project1;
 
+import java.nio.ByteBuffer;
+
 /**
  * Utility class to allow the application to send and receive from the server.
  */
@@ -17,6 +19,21 @@ public class ConnectionUtils {
 			return 4*(res + 1);
 		else
 			return realLength;
+	}
+	
+	/**
+	 * merges 'first' and 'second' into one byte[] that has a length divisible by 4
+	 * @param first the first array in the message
+	 * @param second the next array in the message
+	 * @return a byte[] that contains second concatenated to first followed by 0s 
+	 *         to make the total length divisible by 4.
+	 */
+	public static byte[] merge(byte[] first, byte[] second) {
+		int length = first.length + second.length;
+		int padLen = 4 - length % 4;
+		length += padLen == 4 ? 0 : padLen;
+		byte[] combo = ByteBuffer.allocate(length).put(first).put(second).array();
+		return combo;
 	}
 	
 	public static int getProtocolNumber() {
