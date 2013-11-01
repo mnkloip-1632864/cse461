26dbac41e75210c7822b699486cb90b78f805731
ZhitingZhu <zzt0215@uw.edu>
2013-10-31 20:51:35 -0700

    stageA successes

--- project1/src/project1/Project1Main.java
+++ project1/src/project1/Project1Main.java
@@ -1,16 +1,18 @@
 package project1;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class Project1Main {
 
 	public static void main(String[] args) {
 		byte[] receivedPacket = Stage.stageA();
-		ByteBuffer bb = ByteBuffer.wrap(receivedPacket, ConnectionUtils.HEADER_LENGTH, 16);
-		int num = bb.getInt();
-		int len = bb.getInt(4);
-		int udp_port = bb.getInt(8);
-		int secretA = bb.getInt(12);
+		ByteBuffer bb = ByteBuffer.wrap(receivedPacket);
+		System.out.println(Arrays.toString(bb.array()));
+		int num = bb.getInt(12);
+		int len = bb.getInt(16);
+		int udp_port = bb.getInt(20);
+		int secretA = bb.getInt(24);
 		System.out.println("The num is: " + num);
 		System.out.println("The len is: " + len);
 		System.out.println("udp_port is: " + udp_port);
