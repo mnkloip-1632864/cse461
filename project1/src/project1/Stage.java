1fd18e8f59fb1bb2d3fd0bbe7474524baaea4d80
photoszzt <zzt0215@uw.edu>
2013-10-31 17:33:07 -0700

    changes

--- project1/src/project1/Stage.java
+++ project1/src/project1/Stage.java
@@ -6,38 +6,19 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
+import com.sun.corba.se.pept.transport.Connection;
+
 public class Stage {
 	
-	private DatagramSocket dataSocket;
-	private DatagramPacket dataPacket;
-	
-	public Stage() {
-		
+	public static byte[] stageA() {
+		byte[] payload = "hello world/0".getBytes();
+		Connection udpConn = new UDPConnection(ConnectionUtils.INIT_UDP_PORT);
+		udpConn.send(payload);
+		byte[] receivePacket = udpConn.receive();
+		return receivePacket;
 	}
 	
-	public void send() {
-		try{
-			dataSocket = new DatagramSocket(ConnectionUtils.INIT_UDP_PORT);
-			byte[] payload = "hello world/0".getBytes();
-			int unpadLength = ConnectionUtils.HEADER_LENGTH + payload.length;
-			byte[] header = ConnectionUtils.constructHeader(unpadLength, 0, (short)1);
-			byte[] packet = new byte[10];
-			dataPacket = new DatagramPacket(packet,
-					                        packet.length,
-					                        InetAddress.getByName(Connection.HOST),
-					                        ConnectionUtils.INIT_UDP_PORT);
-			dataSocket.send(dataPacket);
-			
-		} catch (SocketException se) {
-			System.err.println(se.toString());
-			se.printStackTrace();
-		} catch (IOException ie) {
-			System.err.println(ie.toString());
-			ie.printStackTrace();
-		}
-	}
-	
-	public void receive() {
+	public static byte[] stageB(int num, int len, int udp_port) {
 		
 	}
 	
