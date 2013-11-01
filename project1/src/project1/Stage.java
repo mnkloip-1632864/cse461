f2209ad27192a15b3b8f9f3fefa30231c5fb88b3
photoszzt <zzt0215@uw.edu>
2013-10-31 18:29:03 -0700

    need to finish stage c and d

--- project1/src/project1/Stage.java
+++ project1/src/project1/Stage.java
@@ -5,8 +5,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-
-import com.sun.corba.se.pept.transport.Connection;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Stage {
 	
@@ -15,12 +15,38 @@ public class Stage {
 		Connection udpConn = new UDPConnection(ConnectionUtils.INIT_UDP_PORT);
 		udpConn.send(payload);
 		byte[] receivePacket = udpConn.receive();
+		udpConn.close();
 		return receivePacket;
 	}
 	
 	public static byte[] stageB(int num, int len, int udp_port) {
-		
+		byte[] payload;
+		Connection udpConn = new UDPConnection(udp_port);
+		for (int i = 0; i < num; i++) {
+			payload = ByteBuffer.allocate(len + 4).order(ByteOrder.BIG_ENDIAN).putInt(i).array();
+			udpConn.send(payload);
+			int receivedPacketLength = ConnectionUtils.HEADER_LENGTH + 4;
+			byte[] receivedPacket = udpConn.receive(receivedPacketLength);
+			while (receivedPacket == null ||
+				   ByteBuffer.wrap(receivedPacket, ConnectionUtils.HEADER_LENGTH, 4).getInt() != i) {
+				udpConn.send(payload);
+				receivedPacket = udpConn.receive(receivedPacketLength);
+			}
+		}
+		byte[] received = udpConn.receive();
+		udpConn.close();
+		return received;
 	}
+	
+	public static byte[] stageCAndD(int tcp_port) {
+		Connection tcpConn = new TCPConnection(tcp_port);
+		int receivedLength = ConnectionUtils.HEADER_LENGTH + 16;
+		byte[] receivedPacket = tcpConn.receive(receivedLength);
+		ByteBuffer actualData = ByteBuffer.wrap(receivedPacket, ConnectionUtils.HEADER_LENGTH, 13);
+		int num2 = actualData.getInt();
+		int len2 = actualData.getInt(4);
+	}
+	
 	
 
 }
