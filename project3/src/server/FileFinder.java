a51c45cad871eea1e7a9bd040e0f4746c926bd92
joe3701 <joe3701@gmail.com>
2013-11-27 17:29:54 -0800

    Adding skeleton code to FileFinder

--- project3/src/server/FileFinder.java
+++ project3/src/server/FileFinder.java
@@ -1,5 +1,10 @@
 package server;
 
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * The FileFinder provides a way for clients to find nodes that have a 
  * particular file to download.
@@ -9,8 +14,12 @@ public class FileFinder {
 	// There is only one instance of the FileFinder
 	private static volatile FileFinder INSTANCE = null;
 	
+	private Map<String, List<InetAddress>> files; // Filename -> hosts
+	private Map<InetAddress, List<String>> hosts; // Host -> filenames
+	
 	private FileFinder() {
-		
+		files = new HashMap<String, List<InetAddress>>();
+		hosts = new HashMap<InetAddress, List<String>>();
 	}
 	
 	public static FileFinder getInstance() {
@@ -22,6 +31,19 @@ public class FileFinder {
 			}
 		}
 		return INSTANCE;
+	}
+	
+	/**
+	 * Returns a host address that contains fileName. Returns null if
+	 * the filename is not found.
+	 */
+	public synchronized InetAddress getAddressFor(String fileName) {
+		//TODO: implement
+		return null;
+	}
+	
+	public synchronized void removeAddress(InetAddress address) {
+		//TODO: implement
 	}
 	
 }
