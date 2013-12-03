b1f14acb985bb8c13db869dcece791ebf63160e3
photoszzt <zzt0215@uw.edu>
2013-12-02 16:26:39 -0800

    in progress of generating file list

--- project3/src/client/ClientMain.java
+++ project3/src/client/ClientMain.java
@@ -1,28 +1,45 @@
 package client;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class ClientMain {
+	
+	public static final String FILE_LOCATION="../files";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		setup();
-		while (true) {
-			String commend = getCommend();
-			switch (commend) {
+	    sendListFiles();
+	    getAvailableFiles();
+		/*while (true) {
+			String command = getCommand();
+			switch (command) {
 			}
-		}
+		}*/
 	}
 	
-	public static String getCommend() {
+	/**
+	 * 
+	 * @return
+	 */
+	public static String getCommand() {
 		Scanner s = new Scanner(System.in);
 		return s.next();
 	}
 	
 	/**
-	 * 
+	 * Send the available files in the client. 
 	 */
-	public static void setup() {
+	public static void sendListFiles() {
+		File dir = new File(FILE_LOCATION);
+		File[] availableFiles = dir.listFiles();
+		// try to put all files in the same directory for simplicity
+		for (File file: availableFiles) {
+			System.out.println("File name is: " + file.getName());
+		}
+	}
+	
+	public static void getAvailableFiles() {
 		
 	}
 
