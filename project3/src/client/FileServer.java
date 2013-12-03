e34b7826d185e7cbd7437947a26f9fe479da92d7
joe3701 <joe3701@gmail.com>
2013-12-02 20:05:34 -0800

    Made helper method skeletons for the client FileServer and implemented their run() methods.

--- project3/src/client/FileServer.java
+++ project3/src/client/FileServer.java
@@ -1,13 +1,31 @@
 package client;
 
+import utils.ConnectionUtils;
+import utils.TCPConnection;
+import utils.TCPServerConnection;
+
 /**
  * The FileServer handles requests to the Client for a particular file.
  */
 public class FileServer extends Thread {
 	
+	private TCPServerConnection fileServer;
+	
+	public FileServer() {
+		fileServer = new TCPServerConnection(ConnectionUtils.FILE_SERVER_PORT);
+	}
+	
 	@Override
 	public void run() {
-		
+		while(true) {
+			TCPConnection connection = fileServer.accept();
+			if(connection == null) {
+				continue;
+			}
+			// Spawn a new thread with the given connection
+			FileServerThread client = new FileServerThread(connection);
+			client.start();
+		}
 	}
 	
 }
--- project3/src/client/FileServerThread.java
+++ project3/src/client/FileServerThread.java
@@ -1,14 +1,79 @@
 package client;
 
+import java.io.File;
+
+import utils.TCPConnection;
+
 /**
  * The FileServerThread keeps track of a connection to a particular client
  * and their respective file request.
  */
 public class FileServerThread extends Thread {
 
+	private TCPConnection connection;
+
+	public FileServerThread(TCPConnection connection) {
+		this.connection = connection;
+	}
+
 	@Override
 	public void run() {
+		try {
+			// Receive the name of the file to transmit
+			String fileName = receiveFileName();
+			
+			// Get the file path on the local machine, letting the user 
+			// know if the file does not exist.
+			File f = retrieveFile(fileName);
+			
+			// If the file exists, transmit it.
+			transmitFile(f);
+			
+		} catch(FileRetrievalException fre) {
+			// the file the client requested cannot be found, tell the client
+			sendFileNotFound();
+		} finally {
+			connection.close();
+		}
+	}
+
+	/**
+	 * Receives a fileName from the client. 
+	 * @return the name of the file to transmit.
+	 */
+	private String receiveFileName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Looks up the fileName, returning a File instance for the file if one exists.
+	 * @param fileName the name of the file to look up.
+	 * @return a File object that refers to the specified filename.
+	 * @throws FileRetrievalException if the file could not be found (a mapping doesn't
+	 * 		   exist or the file is no longer on disk at the mapped location).
+	 */
+	private File retrieveFile(String fileName) throws FileRetrievalException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Transmits the contents of 'f' to the client. It sends the file to the client
+	 * in CHUNK_SIZE chunks.
+	 * @param f the file to transmit
+	 */
+	private void transmitFile(File f) {
+		// TODO Auto-generated method stub
 		
 	}
 	
+	/**
+	 * Lets the client know that the requested file could not be found.
+	 */
+	private void sendFileNotFound() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
