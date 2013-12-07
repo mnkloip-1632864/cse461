8c952ffb2072e93e9261177d3b8d3b48744b12ef
ZhitingZhu <zzt0215@uw.edu>
2013-12-07 15:04:09 -0800

    implement client side file retrival

--- project3/src/client/ClientMain.java
+++ project3/src/client/ClientMain.java
@@ -1,18 +1,23 @@
 package client;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 import utils.ConnectionUtils;
+import utils.HeaderException;
 import utils.MessageType;
 import utils.TCPConnection;
 
 public class ClientMain {
 	
-	private static final String FILE_LOCATION = "../files";
+	private static final String FILE_LOCATION = ".." + File.pathSeparator + "files";
 	private static final String SERVER_ADDR = "localhost";
 	private static final int NAMES_PER_LINE = 3;
 	private static TCPConnection connectionToServer;
@@ -56,15 +61,113 @@ public class ClientMain {
 				connectionToPeer = new TCPConnection(nodeIp, ConnectionUtils.FILE_SERVER_PORT);
 				sendFileName(fileToGet);
 				
+				byte[] fileMeta = getFileMeta();
+				if (fileMeta == null) {
+					connectionToPeer.close();
+					break;
+				}
+				ByteBuffer data = ByteBuffer.wrap(fileMeta);
+				
+				long fileSize = data.getLong(0);
+				int numChunks = data.getInt(8);
+				int chunkSize = data.getInt(12);
+				try {
+					if (fileSize <= 0) {
+						throw new FileTransmissionException("File size received is incorrect.");
+						
+					}
+					if (numChunks <= 0) {
+						throw new FileTransmissionException("Num chunks received is incorrect.");
+					}
+					if (chunkSize <= 0) {
+						throw new FileTransmissionException("Chunk size received is incorrect.");
+					}
+				} catch (FileTransmissionException e) {
+					// TODO Auto-generated catch block
+					cleanupPeerConn("File meta data received is incorrect!");
+					e.printStackTrace();
+				}
+				
+				// retrieve the file and store it to disk
+				retrieveAndStoreFile(fileSize, numChunks, chunkSize, fileToGet);
 				
 			} else {
-				byte[] header = ConnectionUtils.constructHeader(0, MessageType.TERMINATE);
-				connectionToServer.send(header);
+				byte[] terminate = ConnectionUtils.constructTerminateMessage("User is done!");
+				connectionToServer.send(terminate);
 				connectionToServer.close();
 				break;
 			}
 		}
 		s.close();
+	}
+	
+	public static void retrieveAndStoreFile(long fileSize, int numChunks, int chunkSize, String fileToGet) {
+		long bytesReceived = 0;
+		BufferedOutputStream bufferedOut = null;
+		try {
+			FileOutputStream out = new FileOutputStream(FILE_LOCATION + File.pathSeparator + fileToGet);
+			bufferedOut = new BufferedOutputStream(out);
+			for (int i = 0; i < numChunks; i++) {
+				byte[] header = connectionToPeer.receive(ConnectionUtils.HEADER_SIZE);
+				ByteBuffer buf = ByteBuffer.wrap(header);
+				ConnectionUtils.checkMagic(buf);
+				int payloadLen = buf.getInt(4);
+				byte type = buf.get(8);
+				byte[] message = connectionToPeer.receive(payloadLen);
+				if (type == MessageType.TERMINATE) {
+					System.out.println(new String(message));
+					return;
+				} else if (type == MessageType.FILE_DATA) {
+					bufferedOut.write(message);
+					bytesReceived += payloadLen;
+				} else {
+					throw new HeaderException("Wrong message type!");
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (bufferedOut != null) {
+				try {
+					bufferedOut.flush();
+					bufferedOut.close();
+				} catch (IOException e) {}
+			}
+		}
+		
+	}
+	
+	public static byte[] getFileMeta() {
+		byte[] header = connectionToPeer.receive(ConnectionUtils.HEADER_SIZE);
+		ByteBuffer buf = ByteBuffer.wrap(header);
+		ConnectionUtils.checkMagic(buf);
+		int payloadLen = buf.getInt(4);
+		byte type = buf.get(8);
+		byte[] message = connectionToPeer.receive(payloadLen);
+		
+		if (type == MessageType.TERMINATE) {
+			System.out.println("Transmission terminated: " + new String(message));
+		} else if (type == MessageType.FILE_META) {
+			if (payloadLen != 16) {
+				throw new HeaderException("Wrong payload length!");
+			}
+			return message;
+		} else {
+			System.out.println("Error in retrieving file meta data!");
+		}
+		return null;
+	}
+	
+	public static void cleanupPeerConn(String message) {
+		// TODO
+		byte[] terminate = ConnectionUtils.constructTerminateMessage(message);
+		connectionToPeer.send(terminate);
+		connectionToPeer.close();
 	}
 	
 	public static FileMapping getFileMapping() {
